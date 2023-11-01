927dea2c7ab6defe83c61c3a047b719ca27ace80
romain2518 <rolenoir25@gmail.com>
2023-11-01 12:10:04 +0100

    Add warLeague support on currentWarInfos component

--- utils/clashOfClansApi.js
+++ utils/clashOfClansApi.js
@@ -35,3 +35,40 @@ export async function getMemberList() {
         console.error('An error occured during the API request : ' + error.message);
     }
 };
+
+export async function getWarLeagueGroupInfos() {
+    const config = {
+        headers: {Authorization: `Bearer ${process.env.CLASH_OF_CLAN_JWT}`},
+    };
+
+    try {
+        const response = await fetch(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(process.env.CLAN_TAG)}/currentwar/leaguegroup`, config)
+        if (response.status === 200) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error('An error occured during the API request : ' + response.status);
+            return false;
+        }
+    } catch (error) {
+        console.error('An error occured during the API request : ' + error.message);
+    }
+};
+
+export async function getWarLeagueWarInfos(warTag) {
+    const config = {
+        headers: {Authorization: `Bearer ${process.env.CLASH_OF_CLAN_JWT}`},
+    };
+
+    try {
+        const response = await fetch(`https://api.clashofclans.com/v1/clanwarleagues/wars/${encodeURIComponent(warTag)}`, config)
+        if (response.status === 200) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error('An error occured during the API request : ' + response.status);
+        }
+    } catch (error) {
+        console.error('An error occured during the API request : ' + error.message);
+    }
+};
--- utils/refreshCurrentWarInfos.js
+++ utils/refreshCurrentWarInfos.js
@@ -1,9 +1,37 @@
-import { getCurrentWarInfos } from "./clashOfClansApi.js";
+import { getCurrentWarInfos, getWarLeagueGroupInfos, getWarLeagueWarInfos } from "./clashOfClansApi.js";
 import { displayWarInfo } from "./messageDisplayer.js";
 import 'dotenv/config';
 
 export async function refreshCurrentWarInfos(client) {
-    const data = await getCurrentWarInfos();
+    let data = await getCurrentWarInfos();
+
+    // If clan members key is undefined, it means the clan is either not in war or in a war league
+    if (data.clan.members === undefined) {
+        data = await getWarLeagueGroupInfos();
+        if (data) { // If data is not False, it means the clan is in a war league
+            // Retrieving current round
+            const rounds = data.rounds;
+            let todayRoundIndex
+            rounds.forEach((round, index) => {
+                if (round.warTags[0] !== '#0') {
+                    todayRoundIndex = index;
+                }
+            });
+
+            // Retrieving war league infos of today
+            const todayWarTags = rounds[todayRoundIndex].warTags;
+            let clanTag, opponentTag;
+            for (const warTag of todayWarTags) { // Using a for of loop in order to be able to use await & break statements
+                data = await getWarLeagueWarInfos(warTag);
+    
+                clanTag = data.clan.tag;
+                opponentTag = data.opponent.tag;
+                if (clanTag === process.env.CLAN_TAG || opponentTag === process.env.CLAN_TAG) {
+                    break;
+                }
+            }
+        }
+    }
     const { embedMessages, components } = displayWarInfo(data);
     
     const channel = client.channels.cache.get(process.env.INFOS_CHANNEL_ID);
